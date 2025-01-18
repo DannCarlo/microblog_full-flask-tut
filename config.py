ce45b4b0e4bc68795c50dd83664b792a76459bdc
DannCarlo <danncarloreformado@gmail.com>
2025-01-18 11:48:35 +0800

    Configured app before deploy

--- config.py
+++ config.py
@@ -7,8 +7,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or "http://localhost:9200"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
